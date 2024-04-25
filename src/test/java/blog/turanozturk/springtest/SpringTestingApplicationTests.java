e45bb3f84afcdc98a3fcc2535a9cf4a27acbb2f4
turan.ozturk <turan.ozturk@afad.gov.tr>
2024-04-25 12:03:29 +0300

    first commit

---
+++ src/test/java/blog/turanozturk/springtest/SpringTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package blog.turanozturk.springtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
